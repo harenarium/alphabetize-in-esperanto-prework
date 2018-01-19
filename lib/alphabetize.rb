8d006a479af6649e2fb4131405dfc2bf67cf3985
Caroline Lee <carolinel2610@gmail.com>
2018-01-19 04:00:41 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
 def alphabetize(arr)
+  arr.sort_by{ |phrase|
+    start_value(phrase)
+  }
   # code here
+end
+
+def start_value(phrase)
+  eSPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  (0..27).to_a.each{|x|
+    if phrase.start_with?(eSPERANTO_ALPHABET[x])
+      return x
+  }
 end
