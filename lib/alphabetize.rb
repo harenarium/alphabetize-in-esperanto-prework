9628799e37051924756d6a8246c6cb02616a81b0
Caroline Lee <carolinel2610@gmail.com>
2018-01-19 04:01:09 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -7,8 +7,9 @@ end
 
 def start_value(phrase)
   eSPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  (0..27).to_a.each{|x|
+  (0..27).to_a.each {|x|
     if phrase.start_with?(eSPERANTO_ALPHABET[x])
       return x
+    end
   }
 end
